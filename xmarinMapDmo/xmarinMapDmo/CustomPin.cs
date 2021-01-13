3adbb58bef119735b83d9e59b29338ce0acad86a
Aydın Türker <doktoralban@outlook.com>
2021-01-13 15:37:57 +0300

    Add project files.

---
+++ xmarinMapDmo/xmarinMapDmo/CustomPin.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms.Maps;
+
+namespace xmarinMapDmo
+{
+    public class CustomPin : Pin
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ xmarinMapDmo/xmarinMapDmo/MapPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace xmarinMapDmo
+{
+    public partial class MapPage : ContentPage
+    {
+        public MapPage()
+        {
+            InitializeComponent();
+
+            CustomPin pin = new CustomPin
+            {
+                Type = PinType.Place,
+                Position = new Position(41.02588959503038, 28.974647597264806),
+                Label = "Galata Kulesi",
+                Address = "Galata Kulesi İstanbul Türkiye",
+                Name = "Galata Kulesi",
+                Url = "www.istanbul.com.tr"
+            };
+            customMap.CustomPins = new List<CustomPin> { pin };
+            customMap.Pins.Add(pin);
+            customMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(41.02588959503038, 28.974647597264806), Distance.FromKilometers(1.0)));
+        }
+    }
+}
